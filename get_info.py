3ded013c31e26eada268e3ddcc2debc8dbfa4f2f
Elias <3elias3rd@gmail.com>
2025-12-26 13:41:41 +0400

    Added comments to get_info.py for clarity, and adde main.py where I will run FastAPI code using uvicorn.

--- get_info.py
+++ get_info.py
@@ -2,23 +2,34 @@ import requests
 import os
 from fastapi import HTTPException
 
+# Checks for environment variable named OPENWEATHER_API_KEY.
+# If the environment variable doesn't exist use the default key. 
 API_KEY = os.getenv("OPENWEATHER_API_KEY", "3364a2a3f1e563b6d721f160faa75468")
 
-async def fetch_weather(city: str):
+# Function weather data for a given city.
+# Async allows function to run without blocking other tasks in FastApi server.
+# Returns a dictionary with raw JSON response from weather map.
+async def fetch_weather(city: str) -> dict: 
     url = "http://api.openweathermap.org/data/2.5/weather"
+    
+    # Query parameters to be sent with the requests
     params = {
         "q": city,
         "appid": API_KEY,
         "units": "metric"
     }
 
+    # Send get request to weatherapi with preset parameters.
     response = requests.get(url, params=params)
 
+    # Raise error if get request fails
     if response.status_code != 200:
         raise HTTPException(status_code=response.status_code, detail=f"Failed to fetch weather data: {response.text}")
     
+    #Return response as a Python dict
     return response.json()
 
+# Function to transform raw API response into a simplified format for WeatherResponse model.
 def format_weather_response(data:dict):
     return{
         "city": data["name"],
